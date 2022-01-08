e39f541b0310a46101eefcb685ad82749a815446
Pavel Jacko <beherithrone@gmail.com>
2022-01-08 07:31:28 +0100

    cleanup

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -63,8 +63,8 @@ const config = {
                 items: [
                     {
                         position: 'left',
-                        label: 'v0.33',
-                        href: 'https://github.com/renative-org/renative/releases/tag/0.33.0'
+                        label: 'v0.34',
+                        href: 'https://github.com/renative-org/renative/releases/tag/0.34.0'
                     },
                     {
                         type: 'doc',
