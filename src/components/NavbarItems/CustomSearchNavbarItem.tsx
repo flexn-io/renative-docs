9f74aafe455f9ba648056c141d7e99d60d4f3e83
Marius Žilgužis <marius.zilguzis@flexn.io>
2023-12-15 17:00:58 +0200

    feat: added hightlight of search item

--- src/components/NavbarItems/CustomSearchNavbarItem.tsx
+++ src/components/NavbarItems/CustomSearchNavbarItem.tsx
@@ -14,35 +14,35 @@ const appId = 'FMYKKKF9Q8';
 const apiKey = 'fd3bf87fce092e08e71378cf066734b2';
 const searchClient = algoliasearch(appId, apiKey);
 
-// used to dogfood custom navbar elements are possible
 // see https://github.com/facebook/docusaurus/issues/7227
 export default function CustomSearchNavbarItem(props: {
-  mobile?: boolean;
+    className?: string;
 }): JSX.Element | null {
-  return (
-    <Autocomplete
-                    openOnFocus={true}
-                    getSources={({ query }) => [
-                        {
-                            sourceId: 'products',
-                            getItems() {
-                                return getAlgoliaResults({
-                                    searchClient,
-                                    queries: [
-                                        {
-                                            indexName: 'renative_docs',
-                                            query,
-                                        },
-                                    ],
-                                });
-                            },
-                            templates: {
-                                item({ item, components }) {
-                                    return <ProductItem hit={item} components={components} />;
+    return (
+        <Autocomplete
+            openOnFocus={true}
+            className={props.className}
+            getSources={({ query }) => [
+                {
+                    sourceId: 'products',
+                    getItems() {
+                        return getAlgoliaResults({
+                            searchClient,
+                            queries: [
+                                {
+                                    indexName: 'renative_docs',
+                                    query,
                                 },
-                            },
+                            ],
+                        });
+                    },
+                    templates: {
+                        item({ item, components }) {
+                            return <ProductItem hit={item} components={components} />;
                         },
-                    ]}
-                />
-  );
+                    },
+                },
+            ]}
+        />
+    );
 }
--- src/theme/SearchBar.js
+++ src/theme/SearchBar.js
@@ -36,19 +36,32 @@ export function Autocomplete(props) {
         };
     }, [props]);
 
-    return <div ref={containerRef}/>;
+    return <div ref={containerRef} />;
 }
 
 export function ProductItem({ hit, components }) {
     return (
         <a href={hit.url} className="aa-ItemLink">
             <div className="aa-ItemContent">
-                <div className="aa-ItemTitle">
-                    <components.Highlight hit={hit} attribute="title" />
+                <div className="aa-ItemTitle search_result_title_container">
+                    <img src='/img/page_icon.svg' />
+                    <components.Highlight hit={hit._highlightResult.title} attribute="value" />
                 </div>
                 <div className="aa-ItemContentDescription">
                     <components.Highlight hit={hit} attribute="description" />
                 </div>
+                {hit._highlightResult.sections.map(section => (
+                    (section.name.matchLevel != 'none' || section.content.matchLevel != 'none') &&
+                    <div style={{paddingLeft: '25px'}}>
+                        <div className="aa-ItemTitle search_result_title_container">
+                            <img src='/img/section_icon.svg' />
+                            <components.Highlight hit={section.name} attribute="value" />
+                        </div>
+                        <div className="aa-ItemContentDescription">
+                            <components.Highlight hit={section.content} attribute="value" />
+                        </div>
+                    </div>
+                ))}
             </div>
         </a>
     );
