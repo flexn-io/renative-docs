85021c7bf6bb133b17d039469c31dc3efc062040
Mihai Blaga <mihaiblaga89@gmail.com>
2024-04-15 06:58:02 +0300

    switch from autocomplete to docsearchv3

--- src/components/NavbarItems/CustomSearchNavbarItem.tsx
+++ src/components/NavbarItems/CustomSearchNavbarItem.tsx
@@ -6,12 +6,11 @@
  */
 
 import React from 'react';
-import { Autocomplete, Footer, ProductItem } from '../../theme/SearchBar';
-import { getAlgoliaResults } from '@algolia/autocomplete-js';
-import algoliasearch from 'algoliasearch';
 import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
+import { DocSearch } from '@docsearch/react';
 
-// see https://github.com/facebook/docusaurus/issues/7227
+import '@docsearch/css';
+
 export default function CustomSearchNavbarItem(props: { className?: string }): JSX.Element | null {
     const {
         siteConfig: { customFields },
@@ -21,45 +20,5 @@ export default function CustomSearchNavbarItem(props: { className?: string }): J
     const appId = customFields.ALGOLIA_APP_ID as string;
     const searchKey = customFields.ALGOLIA_SEARCH_KEY as string;
 
-    const searchClient = algoliasearch(appId, searchKey);
-
-    return (
-        <Autocomplete
-            openOnFocus={true}
-            placeholder="Search..."
-            className={props.className}
-            getSources={({ query }) => [
-                {
-                    sourceId: 'products',
-                    getItems() {
-                        return getAlgoliaResults({
-                            searchClient,
-                            queries: [
-                                {
-                                    indexName,
-                                    query,
-                                },
-                            ],
-                        });
-                    },
-                    getItemUrl({ item }) {
-                        return item.url;
-                    },
-                    templates: {
-                        item({ item, components }) {
-                            return <ProductItem hit={item} components={components} />;
-                        },
-                        noResults() {
-                            return (
-                                <div style={{ margin: 'auto', height: '113px', width: '204px' }}>
-                                    <span className="no_results">No results found for </span>
-                                    <strong>"{query}"</strong>
-                                </div>
-                            );
-                        },
-                    },
-                },
-            ]}
-        />
-    );
+    return <DocSearch appId={appId} indexName={indexName} apiKey={searchKey} />;
 }
